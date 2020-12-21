96ff7f9cc8aadfe22993579c23df8967fd14e326
Jackson Sippe <jackson@worxstr.com>
2020-12-21 22:54:03 +0000

    Updated for new name

--- worxstr/asgi.py
+++ worxstr/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for instaworkr project.
+ASGI config for worxstr project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instaworkr.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'worxstr.settings')
 
 application = get_asgi_application()
--- worxstr/wsgi.py
+++ worxstr/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for instaworkr project.
+WSGI config for worxstr project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instaworkr.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'worxstr.settings')
 
 application = get_wsgi_application()
