02d202b6b0d3e75676cf7b130092d5499282db89
Jackson Sippe <jackson123198@gmail.com>
2021-01-22 20:37:53 -0500

    Tie messenger into db

--- app/api/messenger.py
+++ app/api/messenger.py
@@ -5,7 +5,7 @@ from flask import jsonify, request
 from flask_socketio import emit
 from flask_security import current_user, login_required
 
-from app.models import Message, User
+from app.models import Message, User, Conversation
 from app.api import bp
 from app import db, socketio
 
@@ -99,42 +99,24 @@ def populateMessage(message):
 @bp.route('/conversations', methods=['GET'])
 @login_required
 def conversations():
-
-    conversations = db.get('conversations')
-
-    conversations = list(map(populateParticipants, conversations))
+    conversations = db.session.query(Conversation).all()
 
     # List conversations
     if request.method == 'GET':
         return jsonify({
-            'conversations': conversations
+            'conversations': [conversation.to_dict() for conversation in conversations]
         })
 
 @bp.route('/conversations/<conversation_id>', methods=['GET'])
 @login_required
 def conversation(conversation_id):
-
     # Get conversation data along with messages
     if request.method == 'GET':
 
-        cid = int(conversation_id)
-        
-        conversation = search(db.get('conversations'), 'id', int(cid))
-        messages = search_many(db.get('messages'), 'conversation_id', int(cid))
-        conversation = populateParticipants(conversation)
-
-        conversation['messages'] = list(map(populateMessage, messages))
-
+        conversation = db.session.query(Conversation).filter(Conversation.id == conversation_id).one()
         return jsonify({
-            'conversation': conversation
+            'conversation': conversation.to_dict()
         })
-        # current_user.last_message_read_time = datetime.utcnow()
-        # db.session.commit()
-        # messages = current_user.messages_received.order_by(
-        #     Message.timestamp.desc()).all()
-        # return jsonify({
-        #     "event": [x.to_dict() for x in messages]
-        # })
 
 @bp.route('/conversations/<conversation_id>/messages', methods=['GET', 'POST'])
 @login_required
@@ -153,7 +135,7 @@ def messages(conversation_id):
         limit = request.args.get('limit')
 
         # TODO: Query the messages for a given last_id and limit
-        messages = search_many(db.get('messages'), 'conversation_id', int(cid))
+        messages = search_many(test_db.get('messages'), 'conversation_id', int(cid))
 
         return jsonify({
             'messages': list(map(populateMessage, messages))
@@ -169,20 +151,6 @@ def messages(conversation_id):
                 'text': request.json.get('text')
             })
         })
-        
-        # user = db.session.query(User).filter(User.id == recipient).first()
-        # message = Message(author=current_user, recipient=user, body=request.json.get('body'))
-        # db.session.add(message)
-        # db.session.commit()
-
-        # #! Socket.io
-        # socketio.emit('newMessage', {'message': request.json.get('body')})
-
-        # return jsonify({
-        #     "success": True,
-        #     "event": message.to_dict()
-        # })
-
 
 # Send a message through socket.io
 #! NOTE: It may be better to stick with rest for as much as we can.
@@ -195,12 +163,18 @@ def socket_send_message(data):
         data.get('user_id'),
         data.get('message')
     )
+    message = Message (
+        sender_id=data.get('user_id'),
+        body=data.get('message').get('text'),
+    )
+    db.session.add(message)
+    db.session.commit()
 
 def send_message(conversation_id, user_id, message):
 
     # TODO: Get user ID from database by querying for socket session id
 
-    user = search(db.get('users'), 'id', user_id)
+    user = search(test_db.get('users'), 'id', user_id)
 
     new_message = {
         'id': randint(1, 10000),
@@ -209,7 +183,7 @@ def send_message(conversation_id, user_id, message):
         'text': message.get('text')
     }
 
-    db.get('messages').append(new_message)
+    test_db.get('messages').append(new_message)
     
     socketio.emit('message:create', {
         'message': new_message,
--- app/models.py
+++ app/models.py
@@ -51,18 +51,6 @@ class User(db.Model, UserMixin, CustomSerializerMixin):
     active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
     roles = db.relationship('Role', secondary='roles_users', backref=db.backref('users', lazy='dynamic'))
-    messages_sent = db.relationship('Message',
-                                    foreign_keys='Message.sender_id',
-                                    backref='author', lazy='dynamic')
-    messages_received = db.relationship('Message',
-                                        foreign_keys='Message.recipient_id',
-                                        backref='recipient', lazy='dynamic')
-    last_message_read_time = db.Column(db.DateTime)
-
-    def new_messages(self):
-        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
-        return Message.query.filter_by(recipient=self).filter(
-            Message.timestamp > last_read_time).count()
 
 class Organization(db.Model, CustomSerializerMixin):
     __tablename__ = 'organization'
@@ -150,11 +138,22 @@ class EmployeeInfo(db.Model, CustomSerializerMixin):
     hourly_rate = db.Column(db.Numeric)
 
 class Message(db.Model, CustomSerializerMixin):
+    __tablename__ = 'message'
     id = db.Column(db.Integer, primary_key=True)
     sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-
+    conversation_id = db.Column(db.Integer, db.ForeignKey('conversation.id'))
     def __repr__(self):
         return '<Message {}>'.format(self.body)
+
+class Conversation(db.Model, CustomSerializerMixin):
+    __tablename__ = 'conversation'
+    id = db.Column(db.Integer, primary_key=True)
+    participants = db.relationship('User', secondary='user_conversation')
+    messages = db.relationship('Message')
+
+user_conversation_table = db.Table('user_conversation', db.Model.metadata,
+    db.Column('conversation_id', db.Integer, db.ForeignKey('conversation.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+)
