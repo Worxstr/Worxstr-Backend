6bf732432c9b158389e3d46b4199b67c5ce36a19
Jackson Sippe <jackson123198@gmail.com>
2021-09-22 15:25:04 -0600

    Add edit route for organization

--- app/api/organization.py
+++ app/api/organization.py
@@ -4,7 +4,7 @@ from flask_security import (
     roles_required,
     roles_accepted,
 )
-
+from flask import request
 from app import db
 from app.api import bp
 from app.models import Organization
@@ -31,3 +31,19 @@ def get_organization():
         .one()
     )
     return {"organization": result.to_dict()}
+
+@bp.route("/organization", methods=["PATCH"])
+@login_required
+@roles_required("organization_manager")
+def edit_organization():
+    org_wage = float(get_request_json(request, "minimum_wage"))
+
+    db.session.query(Organization).filter(Organization.id == current_user.organization_id).update(
+        {Organization.minimum_wage: org_wage}
+    )
+
+    db.session.commit()
+    org = db.session.query(Organization).filter(Organization.id == current_user.organization_id).one()
+    result = org.to_dict()
+
+    return {"organization": result}, 200
