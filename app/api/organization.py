482591f51b36418b0851144d9d149d521805aaca
Jackson Sippe <jackson123198@gmail.com>
2021-09-22 15:25:50 -0600

    Formatting

--- app/api/organization.py
+++ app/api/organization.py
@@ -32,18 +32,23 @@ def get_organization():
     )
     return {"organization": result.to_dict()}
 
+
 @bp.route("/organization", methods=["PATCH"])
 @login_required
 @roles_required("organization_manager")
 def edit_organization():
     org_wage = float(get_request_json(request, "minimum_wage"))
 
-    db.session.query(Organization).filter(Organization.id == current_user.organization_id).update(
-        {Organization.minimum_wage: org_wage}
-    )
+    db.session.query(Organization).filter(
+        Organization.id == current_user.organization_id
+    ).update({Organization.minimum_wage: org_wage})
 
     db.session.commit()
-    org = db.session.query(Organization).filter(Organization.id == current_user.organization_id).one()
+    org = (
+        db.session.query(Organization)
+        .filter(Organization.id == current_user.organization_id)
+        .one()
+    )
     result = org.to_dict()
 
     return {"organization": result}, 200
