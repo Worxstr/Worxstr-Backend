4555f431e5775a30c204971c15d4ffc0eb3dd262
Jackson Sippe <jackson123198@gmail.com>
2022-02-07 22:02:58 -0700

    Add shift to timecard

--- app/models.py
+++ app/models.py
@@ -290,6 +290,8 @@ class TimeCard(db.Model, CustomSerializerMixin):
     total_payment = db.Column(db.Numeric)
     paid = db.Column(db.Boolean, default=False)
     denied = db.Column(db.Boolean, default=False)
+    shift_id = db.Column(db.Integer, db.ForeignKey("shift.id"))
+    shift = db.relationship("ScheduleShift")
 
     @hybrid_property
     def first_name(self):
