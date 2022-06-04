92b81d9646de9130162d7cfa15c8c0c3d3771c3b
Jackson Sippe <jacksonsippe@gmail.com>
2022-06-04 01:33:42 -0400

    Actually formatted

--- app/api/api.py
+++ app/api/api.py
@@ -17,33 +17,41 @@ from config import Config
 def info():
     return {"app_version": Config.APP_VERSION}
 
-@bp.route("/api/file-upload", methods=['POST'])
+
+@bp.route("/api/file-upload", methods=["POST"])
 def upload():
     object_names = []
-    for file in request.files.getlist('file'):
+    for file in request.files.getlist("file"):
         f = file
         f.save(os.path.join(Config.UPLOAD_FOLDER, secure_filename(f.filename)))
         object_name = f"{uuid.uuid4().hex}/{secure_filename(f.filename)}"
         s3_client = boto3.client(
-            's3',
+            "s3",
             aws_access_key_id=Config.AWS_ACCESS_KEY_ID,
             aws_secret_access_key=Config.AWS_ACCESS_KEY_SECRET,
         )
         try:
-            s3_client.upload_file(os.path.join(Config.UPLOAD_FOLDER, secure_filename(f.filename)), Config.AWS_UPLOADS_BUCKET, object_name)
+            s3_client.upload_file(
+                os.path.join(Config.UPLOAD_FOLDER, secure_filename(f.filename)),
+                Config.AWS_UPLOADS_BUCKET,
+                object_name,
+            )
         except ClientError as e:
             return {"message": "Failed to upload!"}, 500
         object_names.append(object_name)
-    return {'filename': object_names}, 200
+    return {"filename": object_names}, 200
 
-@bp.route("/api/file-download", methods=['GET'])
+
+@bp.route("/api/file-download", methods=["GET"])
 def download():
     bucket_path = get_request_arg(request, "file_name")
     s3_client = boto3.client(
-        's3',
+        "s3",
         aws_access_key_id=Config.AWS_ACCESS_KEY_ID,
         aws_secret_access_key=Config.AWS_ACCESS_KEY_SECRET,
     )
-    with open(os.path.join(Config.DOWNLOAD_FOLDER, os.path.basename(bucket_path)), 'wb') as data:
+    with open(
+        os.path.join(Config.DOWNLOAD_FOLDER, os.path.basename(bucket_path)), "wb"
+    ) as data:
         s3_client.download_fileobj(Config.AWS_UPLOADS_BUCKET, bucket_path, data)
     return send_from_directory(Config.DOWNLOAD_FOLDER, os.path.basename(bucket_path))
