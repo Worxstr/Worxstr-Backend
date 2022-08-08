f673b76f4c6afa00665be1f0cc98051c5b7e281d
Jackson Sippe <jacksonsippe@gmail.com>
2022-08-07 20:04:40 -0600

    Consolidate reports

--- app/api/payment.py
+++ app/api/payment.py
@@ -534,69 +534,6 @@ def get_payment(payment_id):
     return payment.to_dict()
 
 
-@bp.route("/payments/export", methods=["GET"])
-@login_required
-@roles_accepted("organization_manager", "contractor_manager")
-def export_payments():
-    filters = [
-        or_(
-            Payment.sender_dwolla_url == current_user.dwolla_customer_url,
-            Payment.receiver_dwolla_url == current_user.dwolla_customer_url,
-        ),
-        Payment.denied == False,
-    ]
-    payments = (
-        db.session.query(Payment)
-        .filter(*filters)
-        .order_by(desc(Payment.date_created))
-        .all()
-    )
-
-    data = []
-    for payment in payments:
-        payment = payment.to_dict()
-        data.append(
-            {
-                "total": payment.get("total"),
-                "amount": payment.get("amount"),
-                "fee": payment.get("fee"),
-                "date_created": payment.get("date_created"),
-                "date_completed": payment.get("date_completed"),
-            }
-        )
-
-    format = request.args.get("format")
-
-    if format == "csv":
-        filename = "payments_export.csv"
-        mimetype = "text/csv"
-        output = list_to_csv(data)
-
-    elif format == "json":
-        filename = "payments_export.json"
-        mimetype = "application/json"
-        output = json.dumps(data)
-
-    # elif format == 'xlsx':
-    #     filename = "payments_export.xlsx"
-    #     mimetype = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    #     output = list_to_xlsx(data)
-
-    # elif format == 'pdf':
-    #     filename = "payments_export.pdf"
-    #     mimetype = "application/pdf"
-    #     output = list_to_pdf(data)
-
-    else:
-        return "Invalid format provided", 400
-
-    return Response(
-        output,
-        mimetype=mimetype,
-        headers={"Content-disposition": "attachment; filename=" + filename},
-    )
-
-
 @bp.route("/payments/invoices", methods=["POST"])
 @login_required
 def create_invoice():
--- app/api/report.py
+++ app/api/report.py
@@ -1,4 +1,6 @@
 import os
+from requests import Response
+from sqlalchemy import desc
 import xlsxwriter
 from flask_login import current_user, login_required
 from app import db
@@ -6,62 +8,132 @@ from app.api import bp
 from flask import request
 from app.models import ContractorInfo, Invoice, Job, Payment, ScheduleShift, User
 from flask import request, send_from_directory
-from app.utils import OK_RESPONSE, get_request_arg
+from app.utils import (
+    OK_RESPONSE,
+    get_request_arg,
+    list_to_csv,
+    flatten_dict_list,
+    flatten_dict,
+)
 from config import Config
+import json
 
 
-@bp.route("/reports/job", methods=["GET"])
+@bp.route("/reports", methods=["GET"])
 @login_required
-def get_contractor_pay_by_job():
-    start_date = get_request_arg(request, "start_date")
-    end_date = get_request_arg(request, "end_date")
-
-    invoices = (
-        db.session.query(Invoice)
-        .filter(
-            Invoice.job.has(organization_id=current_user.organization_id),
-            Invoice.date_created > start_date,
-            Invoice.date_created < end_date,
-        )
-        .all()
-    )
-    jobs = list(set([invoice.job for invoice in invoices]))
-    filename = "ContractorPayByJob" + start_date + "-" + end_date + ".xlsx"
-    workbook = xlsxwriter.Workbook(os.path.join(Config.DOWNLOAD_FOLDER, filename))
-    for job in jobs:
-        worksheet = workbook.add_worksheet(job.name)
-        row = 0
-        col = 0
-        worksheet.write(row, col, "First Name")
-        worksheet.write(row, col + 1, "Last Name")
-        row += 1
-        invoice_ids = []
-        for invoice in invoices:
-            if invoice.job == job:
-                invoice_ids.append(invoice.id)
+@roles_accepted("organization_manager", "contractor_manager")
+def export_payments():
+    report_type = get_request_arg(request, "report_type")
+    if report_type == "payments":
+        filters = [
+            or_(
+                Payment.sender_dwolla_url == current_user.dwolla_customer_url,
+                Payment.receiver_dwolla_url == current_user.dwolla_customer_url,
+            ),
+            Payment.denied == False,
+        ]
         payments = (
             db.session.query(Payment)
+            .filter(*filters)
+            .order_by(desc(Payment.date_created))
+            .all()
+        )
+
+        data = []
+        for payment in payments:
+            payment = payment.to_dict()
+            data.append(
+                {
+                    "total": payment.get("total"),
+                    "amount": payment.get("amount"),
+                    "fee": payment.get("fee"),
+                    "date_created": payment.get("date_created"),
+                    "date_completed": payment.get("date_completed"),
+                }
+            )
+
+        format = request.args.get("format")
+
+        if format == "csv":
+            filename = "payments_export.csv"
+            mimetype = "text/csv"
+            output = list_to_csv(data)
+
+        elif format == "json":
+            filename = "payments_export.json"
+            mimetype = "application/json"
+            output = json.dumps(data)
+
+        # elif format == 'xlsx':
+        #     filename = "payments_export.xlsx"
+        #     mimetype = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        #     output = list_to_xlsx(data)
+
+        # elif format == 'pdf':
+        #     filename = "payments_export.pdf"
+        #     mimetype = "application/pdf"
+        #     output = list_to_pdf(data)
+
+        else:
+            return "Invalid format provided", 400
+
+        return Response(
+            output,
+            mimetype=mimetype,
+            headers={"Content-disposition": "attachment; filename=" + filename},
+        )
+    elif report_type == "time":
+        start_date = get_request_arg(request, "start_date")
+        end_date = get_request_arg(request, "end_date")
+
+        invoices = (
+            db.session.query(Invoice)
             .filter(
-                Payment.invoice_id.in_(invoice_ids),
-                Payment.date_created > start_date,
-                Payment.date_created < end_date,
+                Invoice.job.has(organization_id=current_user.organization_id),
+                Invoice.date_created > start_date,
+                Invoice.date_created < end_date,
             )
             .all()
         )
-        recipient_dwolla_urls = list(
-            set([payment.receiver_dwolla_url for payment in payments])
-        )
-        recipients = list(
-            set(
-                db.session.query(User, ContractorInfo)
-                .filter(ContractorInfo.dwolla_customer_url.in_(recipient_dwolla_urls))
+        jobs = list(set([invoice.job for invoice in invoices]))
+        filename = "ContractorPayByJob" + start_date + "-" + end_date + ".xlsx"
+        workbook = xlsxwriter.Workbook(os.path.join(Config.DOWNLOAD_FOLDER, filename))
+        for job in jobs:
+            worksheet = workbook.add_worksheet(job.name)
+            row = 0
+            col = 0
+            worksheet.write(row, col, "First Name")
+            worksheet.write(row, col + 1, "Last Name")
+            row += 1
+            invoice_ids = []
+            for invoice in invoices:
+                if invoice.job == job:
+                    invoice_ids.append(invoice.id)
+            payments = (
+                db.session.query(Payment)
+                .filter(
+                    Payment.invoice_id.in_(invoice_ids),
+                    Payment.date_created > start_date,
+                    Payment.date_created < end_date,
+                )
                 .all()
             )
-        )
-        for recipient in recipients:
-            recipient = recipient[0]
-            worksheet.write(row, col, recipient.first_name)
-            worksheet.write(row, col + 1, recipient.last_name)
-            row += 1
-    workbook.close()
-    return send_from_directory(Config.DOWNLOAD_FOLDER, filename)
+            recipient_dwolla_urls = list(
+                set([payment.receiver_dwolla_url for payment in payments])
+            )
+            recipients = list(
+                set(
+                    db.session.query(User, ContractorInfo)
+                    .filter(
+                        ContractorInfo.dwolla_customer_url.in_(recipient_dwolla_urls)
+                    )
+                    .all()
+                )
+            )
+            for recipient in recipients:
+                recipient = recipient[0]
+                worksheet.write(row, col, recipient.first_name)
+                worksheet.write(row, col + 1, recipient.last_name)
+                row += 1
+        workbook.close()
+        return send_from_directory(Config.DOWNLOAD_FOLDER, filename)
